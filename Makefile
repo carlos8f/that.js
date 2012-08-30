753100b08100075f139dc3fb335ac88458a70fdd
Carlos Rodriguez <carlos@s8f.org>
2012-08-30 14:35:27 -0700

    add tests for example app

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 test:
-	@./node_modules/.bin/mocha --reporter spec
+	@cd examples/app; npm install
+	@./node_modules/.bin/mocha \
+		--reporter spec \
+		--timeout 10s \
+		--bail
 
 .PHONY: test
---
+++ test/app.js
@@ -0,0 +1,63 @@
+var assert = require('assert')
+  , http = require('http')
+  , spawn = require('child_process').spawn
+  , resolve = require('path').resolve
+
+function get (url, cb) {
+  http.get(url, function (res) {
+    var data = '';
+    res.once('error', cb);
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on('end', function () {
+      cb(null, res, data);
+    });
+  });
+}
+
+describe('app example', function () {
+  var proc;
+
+  before(function (done) {
+    proc = spawn('node', ['server'], {cwd: resolve(__dirname, '../examples/app')});
+    proc.stdout.on('data', function (chunk) {
+      if (chunk.toString().match(/listening/)) {
+        done();
+      }
+    });
+    process.once('exit', function () {
+      proc.kill();
+    });
+  });
+
+  it('serves homepage', function (done) {
+    get('http://localhost:3000/', function (err, res, data) {
+      assert.ifError(err);
+      assert.equal(res.statusCode, 200);
+      assert(res.headers['content-type'].match(/^text\/html/));
+      assert(data.match(/hello world/));
+      done();
+    });
+  });
+
+  it('serves /about', function (done) {
+    get('http://localhost:3000/about', function (err, res, data) {
+      assert.ifError(err);
+      assert.equal(res.statusCode, 200);
+      assert(res.headers['content-type'].match(/^text\/html/));
+      assert(data.match(/we are awesome/));
+      done();
+    });
+  });
+
+  it('serves /style.css', function (done) {
+    get('http://localhost:3000/style.css', function (err, res, data) {
+      assert.ifError(err);
+      assert.equal(res.statusCode, 200);
+      assert(res.headers['content-type'].match(/^text\/css/));
+      assert(data.match(/background\-color/));
+      done();
+    });
+  });
+});

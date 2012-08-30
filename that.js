659ffa2d3839e00f875d40bdbc6558601fe076e6
Carlos Rodriguez <carlos@s8f.org>
2012-08-30 15:38:41 -0700

    more trumpety

--- that.js
+++ that.js
@@ -2,5 +2,6 @@
 // copyright (c) 2012 Carlos Rodriguez
 // MIT Licensed
 
+// **** BEGIN IMPLEMENTATION ****
 module.exports = new(require('events').EventEmitter);
 module.exports.setMaxListeners(0);
